1fb436715ed242bff5e1b893a68e41e23f736eef
Hyun Woo <hwsa10041@gmail.com>
2023-07-09 14:33:43 +0900

    feat : versionAPI - DTO 구현

---
+++ src/main/java/com/moim/backend/domain/admin/request/VersionServiceRequest.java
@@ -0,0 +1,21 @@
+package com.moim.backend.domain.admin.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class VersionServiceRequest {
+
+    @Getter
+    @NoArgsConstructor
+    public static class Update {
+        private String version;
+        private String updateAdminName;
+
+        @Builder
+        private Update(String version, String updateAdminName) {
+            this.version = version;
+            this.updateAdminName = updateAdminName;
+        }
+    }
+}
