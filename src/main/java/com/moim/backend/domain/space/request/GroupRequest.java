8b455f1151471747f0ac3a22d22502b5da1be910
Hyun Woo <hwsa10041@gmail.com>
2023-07-15 16:20:26 +0900

    feat : 모임 참여 API DTO 구현 및 코드 추가

--- src/main/java/com/moim/backend/domain/space/request/GroupRequest.java
+++ src/main/java/com/moim/backend/domain/space/request/GroupRequest.java
@@ -1,6 +1,7 @@
 package com.moim.backend.domain.space.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,35 @@ public class GroupRequest {
             return GroupServiceRequest.Create.builder()
                     .name(name)
                     .date(date)
+                    .build();
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Participate {
+        @NotNull
+        private Long groupId;
+
+        @NotNull
+        private Double latitude;
+
+        @NotNull
+        private Double longitude;
+
+        @NotNull
+        private String transportation;
+
+        private String password;
+
+        public GroupServiceRequest.Participate toServiceRequest() {
+            return GroupServiceRequest.Participate.builder()
+                    .groupId(groupId)
+                    .latitude(latitude)
+                    .longitude(longitude)
+                    .transportation(transportation)
+                    .password(password)
                     .build();
         }
     }
--- src/main/java/com/moim/backend/global/common/Result.java
+++ src/main/java/com/moim/backend/global/common/Result.java
@@ -6,7 +6,11 @@ import lombok.Getter;
 public enum Result {
 
     OK(0, "성공"),
-    FAIL(-1, "실패");
+    FAIL(-1, "실패"),
+
+    // Space
+    NOT_FOUND_GROUP(-1001, "존재하지 않는 그룹입니다."),
+    INVALID_TRANSPORTATION(-1002, "잘못된 이동수단 입니다.");
 
     private final int code;
     private final String message;
