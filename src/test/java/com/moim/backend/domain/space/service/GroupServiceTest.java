7de7c4d8b986253957c7cfb64f16a2490618bc62
Hyun Woo <hwsa10041@gmail.com>
2023-07-16 12:10:29 +0900

    test : 유저 연동으로 인한 모임 API 테스트 전체 수정
    - Service 테스트에서 User를 통한 테스트로 변경
    - Controller 파라미터값 User 추가
    - 기존 NPE 관련 부족했던 Exception 테스트 추가
    - date 부분 변경될 로직에 맞춰 테스트 수정

--- src/test/java/com/moim/backend/domain/space/service/GroupServiceTest.java
+++ src/test/java/com/moim/backend/domain/space/service/GroupServiceTest.java
@@ -6,6 +6,8 @@ import com.moim.backend.domain.space.entity.Groups;
 import com.moim.backend.domain.space.entity.Participation;
 import com.moim.backend.domain.space.request.GroupRequest;
 import com.moim.backend.domain.space.response.GroupResponse;
+import com.moim.backend.domain.user.entity.Users;
+import com.moim.backend.domain.user.repository.UserRepository;
 import com.moim.backend.global.common.Result;
 import com.moim.backend.global.common.exception.CustomException;
 import org.junit.jupiter.api.DisplayName;
@@ -17,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -36,33 +38,80 @@ class GroupServiceTest {
     @Autowired
     private ParticipationRepository participationRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @DisplayName("하나의 유저가 새로운 모임을 생성한다.")
     @Test
     void create() {
         // given
+        Users user = userRepository.save(
+                Users.builder()
+                        .email("test@test.com")
+                        .name("테스트 이름")
+                        .build()
+        );
+
         GroupRequest.Create request = new GroupRequest.Create(
-                "테스트 그룹", LocalDateTime.of(2023, 7, 14, 15, 0)
+                "테스트 그룹", "2023-07-14"
         );
 
         // when
-        GroupResponse.Create response = groupService.createGroup(request.toServiceRequest());
+        GroupResponse.Create response = groupService.createGroup(request.toServiceRequest(), user);
 
         // then
         assertThat(response)
                 .extracting("groupId", "adminId", "name", "date", "fixedPlace")
-                .contains(1L, response.getAdminId(), "테스트 그룹", "2023-07-14", "none");
+                .contains(1L, user.getUserId(), "테스트 그룹", "2023-07-14", "none");
+    }
+
+    @DisplayName("하나의 유저가 새로운 모임을 생성할때 날짜는 입력하지 않는다.")
+    @Test
+    void createNotEnteredDate() {
+        // given
+        Users user = userRepository.save(
+                Users.builder()
+                        .email("test@test.com")
+                        .name("테스트 이름")
+                        .build()
+        );
+
+        GroupRequest.Create request = new GroupRequest.Create("테스트 그룹", null);
+
+        // when
+        GroupResponse.Create response = groupService.createGroup(request.toServiceRequest(), user);
+
+        // then
+        assertThat(response.getGroupId()).isNotNull();
+        assertThat(response)
+                .extracting("adminId", "name", "date", "fixedPlace")
+                .contains(user.getUserId(), "테스트 그룹", "none", "none");
     }
 
     @DisplayName("하나의 유저가 하나의 그룹에 참가한다.")
     @Test
     void participate() {
         // given
+        Users user = userRepository.save(
+                Users.builder()
+                        .email("test@test.com")
+                        .name("테스트 이름")
+                        .build()
+        );
+
+        Users participateUser = userRepository.save(
+                Users.builder()
+                        .email("test2@test.com")
+                        .name("테스트 이름2")
+                        .build()
+        );
+
         Groups saveGroup = groupRepository.save(
                 Groups.builder()
-                        .adminId(1L)
-                        .name("모임장")
+                        .adminId(user.getUserId())
+                        .name("테스트 그룹")
                         .place("none")
-                        .date(LocalDateTime.of(2023, 7, 12, 13, 5))
+                        .date(LocalDate.of(2023, 7, 23))
                         .build()
         );
 
@@ -70,7 +119,60 @@ class GroupServiceTest {
                 new GroupRequest.Participate(saveGroup.getGroupId(), 37.5660, 126.9784, "BUS", "2345");
 
         // when
-        GroupResponse.Participate response = groupService.participateGroup(request.toServiceRequest());
+        GroupResponse.Participate response =
+                groupService.participateGroup(request.toServiceRequest(), participateUser);
+
+        // then
+        assertThat(response.getParticipationId()).isNotNull();
+        assertThat(response)
+                .extracting(
+                        "groupId", "userId",
+                        "userName", "latitude", "longitude",
+                        "transportation"
+                )
+                .contains(
+                        saveGroup.getGroupId(), participateUser.getUserId(),
+                        participateUser.getName(), 37.5660, 126.9784,
+                        "BUS"
+                );
+
+        Participation participation = participationRepository.findById(response.getParticipationId()).get();
+        assertThat(participation.getPassword()).isEqualTo(encrypt("2345"));
+    }
+
+    @DisplayName("하나의 유저가 하나의 그룹에 참가할때 비밀번호를 입력하지 않는다.")
+    @Test
+    void participateNotEnteredPassword() {
+        // given
+        Users user = userRepository.save(
+                Users.builder()
+                        .email("test@test.com")
+                        .name("테스트 이름")
+                        .build()
+        );
+
+        Users participateUser = userRepository.save(
+                Users.builder()
+                        .email("test2@test.com")
+                        .name("테스트 이름2")
+                        .build()
+        );
+
+        Groups saveGroup = groupRepository.save(
+                Groups.builder()
+                        .adminId(user.getUserId())
+                        .name("테스트 그룹")
+                        .place("none")
+                        .date(LocalDate.of(2023, 7, 23))
+                        .build()
+        );
+
+        GroupRequest.Participate request =
+                new GroupRequest.Participate(saveGroup.getGroupId(), 37.5660, 126.9784, "BUS", null);
+
+        // when
+        GroupResponse.Participate response =
+                groupService.participateGroup(request.toServiceRequest(), participateUser);
 
         // then
         assertThat(response)
@@ -79,10 +181,14 @@ class GroupServiceTest {
                         "userName", "latitude", "longitude",
                         "transportation"
                 )
-                .contains(1L, 1L, 1L, "JWT 미구현", 37.5660, 126.9784, "BUS");
+                .contains(
+                        1L, saveGroup.getGroupId(), participateUser.getUserId(),
+                        participateUser.getName(), 37.5660, 126.9784,
+                        "BUS"
+                );
 
         Participation participation = participationRepository.findById(response.getParticipationId()).get();
-        assertThat(participation.getPassword()).isEqualTo(encrypt("2345"));
+        assertThat(participation.getPassword()).isNull();
     }
 
     @DisplayName("하나의 유저가 하나의 그룹에 참가할때 이동수단이 BUS, SUBWAY 가 아닌 잘못된 코드를 보내면 Exception 이 발생한다.")
@@ -94,7 +200,14 @@ class GroupServiceTest {
                         .adminId(1L)
                         .name("모임장")
                         .place("none")
-                        .date(LocalDateTime.of(2023, 7, 12, 13, 5))
+                        .date(LocalDate.of(2023, 7, 12))
+                        .build()
+        );
+
+        Users user = userRepository.save(
+                Users.builder()
+                        .email("test@test.com")
+                        .name("테스트 이름")
                         .build()
         );
 
@@ -102,7 +215,7 @@ class GroupServiceTest {
                 = new GroupRequest.Participate(saveGroup.getGroupId(), 37.5660, 126.9784, "TAXI", "abc123");
 
         // when // then
-        assertThatThrownBy(() -> groupService.participateGroup(request.toServiceRequest()))
+        assertThatThrownBy(() -> groupService.participateGroup(request.toServiceRequest(), user))
                 .extracting("result.code", "result.message")
                 .contains(-1002, "잘못된 이동수단 입니다.");
     }
