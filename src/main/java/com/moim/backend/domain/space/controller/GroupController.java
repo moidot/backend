653a9493b44631b0a5cdbca4aa369a20d8b1f965
Hyun Woo <hwsa10041@gmail.com>
2023-07-16 12:13:44 +0900

    feat : 모임 생성, 모임 참가 API 변경
    - 두 API 동일하게 JWT 토큰을 이용해 User 정보를 가져오게 변경
    - NPE가 발생하지 않도록 구현
    - 모임 날짜를 입력받고 저장하는 로직 수정

--- src/main/java/com/moim/backend/domain/space/controller/GroupController.java
+++ src/main/java/com/moim/backend/domain/space/controller/GroupController.java
@@ -3,6 +3,8 @@ package com.moim.backend.domain.space.controller;
 import com.moim.backend.domain.space.request.GroupRequest;
 import com.moim.backend.domain.space.response.GroupResponse;
 import com.moim.backend.domain.space.service.GroupService;
+import com.moim.backend.domain.user.entity.Users;
+import com.moim.backend.global.auth.Login;
 import com.moim.backend.global.common.CustomResponseEntity;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,15 +20,15 @@ public class GroupController {
 
     @PostMapping("/api/v1/group")
     public CustomResponseEntity<GroupResponse.Create> createGroup(
-            @RequestBody @Valid GroupRequest.Create request
+            @RequestBody @Valid GroupRequest.Create request, @Login Users user
     ) {
-        return CustomResponseEntity.success(groupService.createGroup(request.toServiceRequest()));
+        return CustomResponseEntity.success(groupService.createGroup(request.toServiceRequest(), user));
     }
 
     @PostMapping("/api/v1/group/participate")
     public CustomResponseEntity<GroupResponse.Participate> participateGroup(
-             @RequestBody @Valid GroupRequest.Participate request
+            @RequestBody @Valid GroupRequest.Participate request, @Login Users user
     ) {
-        return CustomResponseEntity.success(groupService.participateGroup(request.toServiceRequest()));
+        return CustomResponseEntity.success(groupService.participateGroup(request.toServiceRequest(), user));
     }
 }
