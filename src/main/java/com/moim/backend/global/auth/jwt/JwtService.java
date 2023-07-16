e946c4b80cfdfbf987fbb8e281e49573d5510a4d
yujung7768903 <yu-jung31476@naver.com>
2023-07-16 19:47:24 +0900

    refactor: JwtService에서 불필요한 주석 제거

--- src/main/java/com/moim/backend/global/auth/jwt/JwtService.java
+++ src/main/java/com/moim/backend/global/auth/jwt/JwtService.java
@@ -45,22 +45,6 @@ public class JwtService implements InitializingBean {
                 .setExpiration(validity)
                 .compact();
     }
-//
-//    public boolean isValidated(String token) {
-//        try {
-//            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-//            return true;
-//        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-//            log.info("잘못된 JWT 서명입니다.");
-//        } catch (ExpiredJwtException e) {
-//            log.info("만료된 JWT 토큰입니다.");
-//        } catch (UnsupportedJwtException e) {
-//            log.info("지원되지 않는 JWT 토큰입니다.");
-//        } catch (IllegalArgumentException e) {
-//            log.info("JWT 토큰이 잘못되었습니다.");
-//        }
-//        return false;
-//    }
 
     public boolean isValidated(String token) throws Exception {
         try {
