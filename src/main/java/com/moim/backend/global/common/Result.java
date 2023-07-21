c3a8794f846f9ac55998fd69bd20b83a0cb1c43e
Hyun Woo <hwsa10041@gmail.com>
2023-07-21 14:18:00 +0900

    feat : 모임원 내보내기 Result 코드 추가

--- src/main/java/com/moim/backend/global/common/Result.java
+++ src/main/java/com/moim/backend/global/common/Result.java
@@ -12,7 +12,8 @@ public enum Result {
     NOT_FOUND_GROUP(-1001, "존재하지 않는 그룹입니다."),
     INVALID_TRANSPORTATION(-1002, "잘못된 이동수단 입니다."),
     NOT_FOUND_PARTICIPATE(-1003, "존재하지 않는 참여자 정보 입니다."),
-    NOT_MATCHED_PARTICIPATE(-1004, "자신의 참여 정보가 아닙니다.");
+    NOT_MATCHED_PARTICIPATE(-1004, "자신의 참여 정보가 아닙니다."),
+    NOT_ADMIN_USER(-1005,"해당 유저는 그룹의 어드민이 아닙니다.");
 
     private final int code;
     private final String message;
