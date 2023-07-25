f4b9308bb243baa7f395a13307d42a82731281f7
Hyun Woo <hwsa10041@gmail.com>
2023-07-25 15:00:50 +0900

    fix : 제외됐던 Repository 추가

---
+++ src/main/java/com/moim/backend/domain/space/repository/ParticipationRepository.java
@@ -0,0 +1,21 @@
+package com.moim.backend.domain.space.repository;
+
+import com.moim.backend.domain.space.entity.Groups;
+import com.moim.backend.domain.space.entity.Participation;
+import com.moim.backend.domain.space.response.MiddlePoint;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+
+    @Query("SELECT "
+            + "new com.moim.backend.domain.space.response.MiddlePoint(SUM(p.latitude), SUM(p.longitude), COUNT(*)) "
+            + "FROM Participation p "
+            + "GROUP BY p.group "
+            + "HAVING p.group = :group")
+    MiddlePoint getMiddlePoint(@Param("group") Groups group);
+
+}
