8b3269f1a9b5e1f58332386c7dfb787fcbd1cc96
Hyun Woo <hwsa10041@gmail.com>
2023-08-19 14:36:53 +0900

    docs : Docs 최신화

--- src/test/java/com/moim/backend/domain/ControllerTestSupport.java
+++ src/test/java/com/moim/backend/domain/ControllerTestSupport.java
@@ -1,18 +1,13 @@
 package com.moim.backend.domain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.moim.backend.domain.admin.controller.VersionController;
-import com.moim.backend.domain.admin.service.VersionService;
 import com.moim.backend.domain.groupvote.controller.VoteController;
 import com.moim.backend.domain.space.controller.GroupController;
-import com.moim.backend.domain.space.service.GroupService;
 import com.moim.backend.domain.user.controller.UserController;
-import com.moim.backend.domain.user.service.UserService;
 import com.moim.backend.global.auth.LoginArgumentResolver;
 import com.moim.backend.global.auth.LoginInterceptor;
 import com.moim.backend.global.auth.jwt.JwtService;
 import com.moim.backend.global.config.WebConfig;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -22,7 +17,6 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = {
-        VersionController.class,
         GroupController.class,
         UserController.class,
         VoteController.class
@@ -36,9 +30,6 @@ public abstract class ControllerTestSupport {
 
     @Autowired
     protected ObjectMapper objectMapper;
-
-    @MockBean
-    protected VersionController versionController;
 
     @MockBean
     protected GroupController groupController;
