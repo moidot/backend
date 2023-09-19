ba1a2582eacd7fc2a0ff3757e35da8b672e2b39c
Hyun Woo <hwsa10041@gmail.com>
2023-09-19 15:36:24 +0900

    feat : Controller 단위 ms 측정 및 메서드 실행 및 종료 AOP 적용

--- src/main/java/com/moim/backend/global/config/AppConfig.java
+++ src/main/java/com/moim/backend/global/config/AppConfig.java
@@ -5,8 +5,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AppConfig {
     @PersistenceContext
     private EntityManager entityManager;
