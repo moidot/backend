3d8c305832f35110b22833e61e6004ea4184c8ab
Hyun Woo <hwsa10041@gmail.com>
2023-09-19 15:37:16 +0900

    chore : Main에 proxy 어노테이션 제거

--- src/main/java/com/moim/backend/BackendApplication.java
+++ src/main/java/com/moim/backend/BackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
