d9f518629cefa3c706c0a0017cd07246aa5aeb97
yujung <yu-jung0422@hankookilbo.com>
2023-10-26 16:18:45 +0900

    refactor: 시간 측정 로그를 위한 어노테이션 추가, 로그 설정 변경

--- src/main/java/com/moim/backend/global/aspect/PerformanceAspect.java
+++ src/main/java/com/moim/backend/global/aspect/PerformanceAspect.java
@@ -4,14 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @Slf4j
 public class PerformanceAspect {
-    @Around("execution(* com.moim.backend.domain..controller.*.*(..))")
-    public Object measureMethodExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("execution(* com.moim.backend.domain..controller.*.*(..)) || @annotation(com.moim.backend.global.aspect.TimeCheck)")
+    public Object measureClassMethodExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
         Object returnValue = joinPoint.proceed();
         long totalTime = System.currentTimeMillis() - startTime;
