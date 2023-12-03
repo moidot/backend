5a91db462ba0bcc1564c2dbea3e81e64cc354d70
yujung7768903 <yu-jung31476@naver.com>
2023-12-03 18:16:08 +0900

    test: 투표를 하지 않은 사람이 0명일 때 에러나는지 테스트하는 테스트 제거
    * 클라이언트 측에 투표 인원이 0명일 때도 해당 API를 호출하는 경우가 있을 것 같아서 에러보다 빈 리스트를 반환하도록 수정

--- src/test/java/com/moim/backend/domain/groupvote/service/VoteServiceTest.java
+++ src/test/java/com/moim/backend/domain/groupvote/service/VoteServiceTest.java
@@ -291,24 +291,23 @@ class VoteServiceTest {
         // given
         Users user = savedUser("test@test.com", "테스트");
         Users admin = savedUser("admin@admin.com", "어드민");
-        System.out.println("유저 및 관리자까지 생성 후 저장");
+
         Groups group = savedGroup(admin.getUserId(), "테스트 그룹");
 
         Participation par1 = savedParticipation(admin, group, "어드민", "테스트", 123.123, 123.123, "PUBLIC");
         Participation par2 = savedParticipation(user, group, "일반인", "테스트", 123.123, 123.123, "PUBLIC");
-        System.out.println("참가자 정보 생성 후 저장");
+
         BestPlace bestPlace1 = saveBestPlace(group, "강남역", 123.123456, 123.123456);
         BestPlace bestPlace2 = saveBestPlace(group, "역삼역", 123.123456, 123.123456);
         BestPlace bestPlace3 = saveBestPlace(group, "신논현역", 123.123456, 123.123456);
 
         Vote vote = saveVote(group.getGroupId(), true, true, LocalDateTime.of(2023, 8, 3, 12, 0, 0));
-        System.out.println("투표 생성 후 저장");
 
         SelectPlace selectPlace1 = saveSelectPlace(user, bestPlace1, vote);
         SelectPlace selectPlace2 = saveSelectPlace(user, bestPlace3, vote);
         SelectPlace selectPlace3 = saveSelectPlace(admin, bestPlace1, vote);
         SelectPlace selectPlace4 = saveSelectPlace(admin, bestPlace2, vote);
-        System.out.println("투표 진행 후 저장");
+
         em.flush();
         em.clear();
 
@@ -316,7 +315,7 @@ class VoteServiceTest {
         VoteSelectPlaceUserResponse response = voteService.readSelectPlaceUsers(
                 group.getGroupId(), bestPlace1.getBestPlaceId()
         );
-        System.out.println("투표 결과 조회");
+
         // then
         assertThat(response.getVoteParticipations())
                 .extracting("participationId", "userId", "nickName", "isAdmin")
@@ -325,34 +324,6 @@ class VoteServiceTest {
                         tuple(par2.getParticipationId(), par2.getUserId(), par2.getUserName(), false)
 
                 );
-    }
-
-    @DisplayName("유저가 해당 장소에 투표한 사람들의 목록을 조회한다.")
-    @Test
-    void readSelectPlaceUsersWithNotVotedPlaceThrowException() {
-        // given
-        Users user = savedUser("test@test.com", "테스트");
-        Users admin = savedUser("admin@admin.com", "어드민");
-
-        Groups group = savedGroup(admin.getUserId(), "테스트 그룹");
-
-        Participation participation1 = savedParticipation(admin, group, "어드민", "테스트", 123.123, 123.123, "PUBLIC");
-        Participation participation2 = savedParticipation(user, group, "일반인", "테스트", 123.123, 123.123, "PUBLIC");
-
-        BestPlace bestPlace1 = saveBestPlace(group, "강남역", 123.123456, 123.123456);
-        BestPlace bestPlace2 = saveBestPlace(group, "역삼역", 123.123456, 123.123456);
-        BestPlace bestPlace3 = saveBestPlace(group, "신논현역", 123.123456, 123.123456);
-
-        Vote vote = saveVote(group.getGroupId(), true, true, LocalDateTime.of(2023, 8, 3, 12, 0, 0));
-
-        em.flush();
-        em.clear();
-
-        // when // then
-        assertThatThrownBy(
-                () -> voteService.readSelectPlaceUsers(group.getGroupId(), bestPlace3.getBestPlaceId()))
-                .extracting("result.code", "result.message")
-                .contains(-2006, "해당 장소를 투표한 인원은 0명 입니다.");
     }
 
     @DisplayName("어드민이 자신의 그룹의 투표를 종료하고 가장 높은 선택을 받은 장소가 선정된다.")
