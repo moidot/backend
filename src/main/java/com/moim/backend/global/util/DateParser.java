495b2915241fdb597a63742b55e94cb466ee98dd
1000kkannoo <hwsa10041@gmail.com>
2024-02-18 16:16:28 +0900

    refactor : UserCalendarService API 단위 별 주석 추가 및 날짜 관련 함수 리팩터링

--- src/main/java/com/moim/backend/global/util/DateParser.java
+++ src/main/java/com/moim/backend/global/util/DateParser.java
@@ -1,6 +1,8 @@
 package com.moim.backend.global.util;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 
@@ -20,5 +22,14 @@ public class DateParser {
 
     public static String getDayOfWeek(LocalDateTime date) {
         return date.getDayOfWeek().getDisplayName(SHORT, KOREAN);
+    }
+
+    public static LocalDateTime getEndDate(LocalDateTime startDate) {
+        YearMonth yearMonth = YearMonth.from(startDate);
+        return yearMonth.atEndOfMonth().atTime(23, 59, 59);
+    }
+
+    public static LocalDateTime getEndDateTime(LocalDateTime startDateTime) {
+        return startDateTime.with(LocalTime.MAX);
     }
 }
