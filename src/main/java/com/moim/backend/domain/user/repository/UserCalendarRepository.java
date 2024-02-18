46581261260f6d3ef6530effa4137123fe021a32
1000kkannoo <hwsa10041@gmail.com>
2024-02-18 16:17:22 +0900

    refactor : UserCalendarRepository에 @Repository 어노테이션 추가

--- src/main/java/com/moim/backend/domain/user/repository/UserCalendarRepository.java
+++ src/main/java/com/moim/backend/domain/user/repository/UserCalendarRepository.java
@@ -4,11 +4,13 @@ import com.moim.backend.domain.user.entity.UserCalendar;
 import com.moim.backend.domain.user.entity.Users;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserCalendarRepository extends JpaRepository<UserCalendar, Long> {
     List<UserCalendar> findByUserAndDateBetween(Users user, LocalDateTime startDate, LocalDateTime endDate);
 }
