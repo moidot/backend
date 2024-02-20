7537ed929db5c10dcdab28ad8dd728cb752117f9
Hyun Woo <hwsa10041@gmail.com>
2024-02-20 19:34:40 +0900

    fix : Cors 수정

--- src/main/java/com/moim/backend/global/config/CorsConfig.java
+++ src/main/java/com/moim/backend/global/config/CorsConfig.java
@@ -15,6 +15,8 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("https://api.moidot.co.kr/")
                 .allowedOrigins("https://moidot.vercel.app")
                 .allowedOrigins("https://moidot.vercel.app/")
+                .allowedOrigins("https://www.moidot.co.kr")
+                .allowedOrigins("https://www.moidot.co.kr/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
