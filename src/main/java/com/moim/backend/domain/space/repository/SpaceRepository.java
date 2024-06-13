e9d9627cc5ed66d3549a56b92a93ab7437eee093
1000kkannoo <hwsa10041@gmail.com>
2024-06-13 23:00:02 +0900

    fix : 토큰 예외 문제 수정

--- src/main/java/com/moim/backend/domain/space/repository/SpaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.moim.backend.domain.space.repository;
-
-import com.moim.backend.domain.space.entity.Space;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpaceRepository extends JpaRepository<Space, Long>, SpaceCustomRepository {
-}
--- src/main/java/com/moim/backend/domain/subway/response/BestPlaceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.moim.backend.domain.subway.response;
-
-public interface BestPlaceInterface {
-
-    public String getName();
-    public double getLatitude();
-    public double getLongitude();
-
-}
