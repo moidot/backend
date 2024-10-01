283f095c376f3fe1989c66c9b0d1acfb1ba37dd3
yujung7768903 <yu-jung31476@naver.com>
2024-10-01 21:46:23 +0900

    스크립트에 권한 추가 + nginx 설정 제거

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -6,17 +6,17 @@ PROJECT_ROOT="/home/ec2-user/backend"
 JAR_FILE="$PROJECT_ROOT/build/libs/backend-0.0.1-SNAPSHOT.jar"
 
 CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
-TARGET_PORT=0
+TARGET_PORT=8080
 
-echo "> Current port of running WAS is ${CURRENT_PORT}."
-
-if [ ${CURRENT_PORT} -eq 8081 ]; then
-  TARGET_PORT=8082
-elif [ ${CURRENT_PORT} -eq 8082 ]; then
-  TARGET_PORT=8081
-else
-  echo "> No WAS is connected to nginx"
-fi
+#echo "> Current port of running WAS is ${CURRENT_PORT}."
+#
+#if [ ${CURRENT_PORT} -eq 8081 ]; then
+#  TARGET_PORT=8082
+#elif [ ${CURRENT_PORT} -eq 8082 ]; then
+#  TARGET_PORT=8081
+#else
+#  echo "> No WAS is connected to nginx"
+#fi
 
 TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
 
