6bcc89a5103101818bdd46698a8928070f75dd5f
yujung7768903 <yu-jung31476@naver.com>
2024-10-13 20:41:15 +0900

    chore: stop_was.sh 실행 관련 로그 추가

--- scripts/stop_was.sh
+++ scripts/stop_was.sh
@@ -5,6 +5,8 @@ TARGET_PORT=8080
 
 TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
 
+echo $TARGET_PORT
+
 if [ ! -z ${TARGET_PID} ]; then
   echo "> Kill WAS running at ${TARGET_PORT}."
   sudo kill ${TARGET_PID}
