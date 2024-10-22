15ce122a2ef5584ba6f3d908e2f9b3c20a915a66
yujung7768903 <yu-jung31476@naver.com>
2024-10-22 23:21:01 +0900

    docs: swagger 문서 정리

--- src/main/java/com/moim/backend/domain/space/controller/SpaceController.java
+++ src/main/java/com/moim/backend/domain/space/controller/SpaceController.java
@@ -32,7 +32,8 @@ public class SpaceController {
     @PostMapping("")
     @Operation(summary = "모임 생성", description = "모임 생성")
     public CustomResponseEntity<SpaceCreateResponse> createSpace(
-            @RequestBody @Valid SpaceCreateRequest request, @Login Users user
+            @RequestBody @Valid SpaceCreateRequest request,
+            @Parameter(hidden = true) @Login Users user
     ) {
         return CustomResponseEntity.success(spaceService.createSpace(request, user));
     }
@@ -40,6 +41,9 @@ public class SpaceController {
     // 모임 참여자 정보 리스트 조회 API
     @GetMapping("")
     @Operation(summary = "모임 참여자 정보 리스트 조회", description = "모임에 참여하고 있는 모임원 조회")
+    @Parameters({
+            @Parameter(name = "groupId", description = "스페이스 아이디", example = "8")
+    })
     public CustomResponseEntity<SpaceDetailResponse> readParticipateSpaceByRegion(
             @RequestParam Long groupId
     ) {
@@ -49,9 +53,13 @@ public class SpaceController {
     // 모임 이름 수정 API
     @PatchMapping("")
     @Operation(summary = "모임 이름 수정", description = "모임 이름 수정")
+    @Parameters({
+            @Parameter(name = "groupId", description = "스페이스 아이디", example = "8")
+    })
     public CustomResponseEntity<Void> updateSpaceName(
             @RequestParam Long groupId,
-            @RequestBody @Valid SpaceNameUpdateRequest request, @Login Users user
+            @RequestBody @Valid SpaceNameUpdateRequest request,
+            @Parameter(hidden = true) @Login Users user
     ) {
         return CustomResponseEntity.success(spaceService.updateSpaceName(groupId, request, user));
     }
@@ -60,16 +68,21 @@ public class SpaceController {
     @PostMapping("/participate")
     @Operation(summary = "모임 참여", description = "모임 참여")
     public CustomResponseEntity<SpaceParticipateResponse> participateSpace(
-            @RequestBody @Valid SpaceParticipateRequest request, @Login Users user
+            @RequestBody @Valid SpaceParticipateRequest request,
+            @Parameter(hidden = true) @Login Users user
     ) {
         return CustomResponseEntity.success(spaceService.participateSpace(request, user));
     }
 
     // 모임 삭제 API
     @DeleteMapping("")
-    @Operation(summary = "모임 삭", description = "모임 삭제")
+    @Operation(summary = "모임 삭제", description = "모임 삭제")
+    @Parameters({
+            @Parameter(name = "groupId", description = "스페이스 아이디", example = "8")
+    })
     public CustomResponseEntity<Void> deleteSpace(
-            @RequestParam Long groupId, @Login Users user
+            @RequestParam Long groupId,
+            @Parameter(hidden = true) @Login Users user
     ) {
         return CustomResponseEntity.success(spaceService.participateDelete(groupId, user));
     }
@@ -81,7 +94,8 @@ public class SpaceController {
             @Parameter(name = "groupId", description = "스페이스 아이디", example = "8")
     })
     public CustomResponseEntity<SpaceParticipationsResponse> getParticipationDetail(
-            @RequestParam Long groupId, @Login Users user
+            @RequestParam Long groupId,
+            @Parameter(hidden = true) @Login Users user
     ) {
         return CustomResponseEntity.success(spaceService.getParticipationDetail(groupId, user));
     }
@@ -91,7 +105,8 @@ public class SpaceController {
     @PatchMapping("/participate")
     @Operation(summary = "내 참여 정보 수정", description = "내 참여 정보 수정")
     public CustomResponseEntity<SpaceParticipateUpdateResponse> participateUpdate(
-            @RequestBody @Valid SpaceParticipateUpdateRequest request, @Login Users user
+            @RequestBody @Valid SpaceParticipateUpdateRequest request,
+            @Parameter(hidden = true) @Login Users user
     ) {
         return CustomResponseEntity.success(spaceService.participateUpdate(request, user));
     }
@@ -99,8 +114,12 @@ public class SpaceController {
     // 모임 나가기 API
     @DeleteMapping("/participate")
     @Operation(summary = "모임 나가기", description = "모임 나가기")
+    @Parameters({
+            @Parameter(name = "participateId", description = "참여 아이디", example = "8")
+    })
     public CustomResponseEntity<SpaceExitResponse> participateExit(
-            @RequestParam Long participateId, @Login Users user
+            @RequestParam Long participateId,
+            @Parameter(hidden = true) @Login Users user
     ) {
         return CustomResponseEntity.success(spaceService.participateExit(participateId, user));
     }
@@ -109,7 +128,7 @@ public class SpaceController {
     @DeleteMapping("/participate/all")
     @Operation(summary = "모임 전체 나가기", description = "모임 전체 나가기")
     public CustomResponseEntity<Void> allParticipateExit(
-            @Login Users user
+            @Parameter(hidden = true) @Login Users user
     ) {
         return CustomResponseEntity.success(spaceService.allParticipateExit(user));
     }
@@ -117,8 +136,12 @@ public class SpaceController {
     // 모임원 내보내기 API
     @DeleteMapping("/participate/removal")
     @Operation(summary = "모임원 내보내기", description = "모임원 내보내기")
+    @Parameters({
+            @Parameter(name = "participateId", description = "참여 아이디", example = "8")
+    })
     public CustomResponseEntity<Void> participateRemoval(
-            @RequestParam Long participateId, @Login Users user
+            @RequestParam Long participateId,
+            @Parameter(hidden = true) @Login Users user
     ) {
         return CustomResponseEntity.success(spaceService.participateRemoval(participateId, user));
     }
@@ -126,6 +149,9 @@ public class SpaceController {
     // 모임 추천 역(랜드마크) 조회하기 API
     @GetMapping("/best-region")
     @Operation(summary = "모임 추천 지역 조회", description = "중간 지역 추천")
+    @Parameters({
+            @Parameter(name = "groupId", description = "스페이스 아이디", example = "8")
+    })
     public CustomResponseEntity<List<PlaceRouteResponse>> getBestRegion(
             @RequestParam Long groupId
     ) {
@@ -136,7 +162,7 @@ public class SpaceController {
     @GetMapping("/participate")
     @Operation(summary = "내 모임 리스트 조회", description = "내가 참여하고 있는 모임 리스트")
     public CustomResponseEntity<List<SpaceMyParticipateResponse>> getMyParticipate(
-            @Login Users user,
+            @Parameter(hidden = true) @Login Users user,
             @RequestParam(required = false) String spaceName,
             @RequestParam(required = false) SpaceFilterEnum filter
     ) {
@@ -158,6 +184,10 @@ public class SpaceController {
     // 닉네임 유효성 체크
     @GetMapping("/nickname")
     @Operation(summary = "닉네임 유효성 체크", description = "스페이스 내에 동일한 닉네임이 있는지 중복 체크")
+    @Parameters({
+            @Parameter(name = "groupId", description = "스페이스 아이디", example = "8"),
+            @Parameter(name = "nickname", description = "중복을 확인하려는 닉네임", example = "모이닷")
+    })
     public CustomResponseEntity<NicknameValidationResponse> checkNicknameValidation(
             @RequestParam Long groupId,
             @RequestParam String nickname
